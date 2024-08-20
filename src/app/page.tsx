7439106e408504d13568d67743a924960fef624e
CrisEmyoi <157163248+CrisEmyoi@users.noreply.github.com>
2024-08-19 18:10:49 -0600

    Pruebas creado nuevos elementos como boton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components/ui/Input";
 import { Label } from "@/components/ui/Label";
+import { Button } from "@/components/ui/Button";
 export default function Example() {
   return (
     <>
@@ -16,7 +17,7 @@ export default function Example() {
           <img
             alt="Your Company"
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto"
+            className="mx-auto h-10 w-auto bg-black"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
@@ -24,6 +25,35 @@ export default function Example() {
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form action="#" method="POST" className="space-y-6">
+            {/* Nombre */}
+            <div>
+              <Label htmlFor="name">Nombre</Label>
+              <div className="mt-2">
+                <Input
+                  placeholder="Nombre"
+                  id="name"
+                  name="name"
+                  type="name"
+                  required
+                  autoComplete="name"
+                />
+              </div>
+            </div>
+            {/* LastName */}
+            <div>
+              <Label htmlFor="lastname">Apellidos</Label>
+              <div className="mt-2">
+                <Input
+                  placeholder="LastName"
+                  id="lasname"
+                  name="lastname"
+                  type="lastname"
+                  required
+                  autoComplete="lastname"
+                />
+              </div>
+            </div>
+            {/* Email */}
             <div>
               <Label htmlFor="emailaddress">Email address</Label>
               <div className="mt-2">
@@ -39,8 +69,16 @@ export default function Example() {
             </div>
 
             <div>
+              <Label>Hola world</Label>
+              <div className="mt-2">
+                <Input placeholder="Tira todo"></Input>
+              </div>
+            </div>
+            {/* Password */}
+            <div>
               <div className="flex items-center justify-between">
                 <Label htmlFor="password">Password</Label>
+                {/* Forgot PassWord */}
                 <div className="text-sm">
                   <a
                     href="#"
@@ -51,23 +89,26 @@ export default function Example() {
                 </div>
               </div>
               <div className="mt-2">
-                <Input placeholder="*******" 
-                id="password"
-                name="password"
-                type="password"
-                required
-                autoComplete="password"/>
+                <Input
+                  placeholder="*******"
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="password"
+                />
               </div>
             </div>
-
-            <div>
-              <button
+            {/* Boton */}
+            <Button>
+              Sign in
+              </Button>            
+              {/* <button
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
-              </button>
-            </div>
+              </button> */}            
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,15 @@
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export function Button(props: Props) {
+  return (
+    <div>
+      <button
+        type="submit"
+        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        {...props}
+      />
+    </div>
+  );
+}
+
+export default Button;
